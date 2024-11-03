36bb7863bc4ab2a8f6d8a88477aed57da3e7d0ee
Chiester556 <joshua.y.kim@student.uts.edu.au>
2024-11-03 23:16:27 +1100

    50 percent commit

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TitleScreenManager : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("PacStudent"); 
+    }
+
+    public void LoadLevel2()
+    {
+    }
+}
